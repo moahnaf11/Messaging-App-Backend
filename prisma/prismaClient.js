8b8c535c2bbbba1e864c9a3ae4e1181de275889e
Ahnaf <mohammadahnaf123@gmail.com>
2024-12-18 15:12:34 +0400

    Connect to proper database
    Connected to proper databases

--- prisma/prismaClient.js
+++ prisma/prismaClient.js
@@ -1,5 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
 
-const prisma = new PrismaClient();
+const databaseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.RENDER_DB
+    : process.env.DATABASE_URL;
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
 
 export { prisma };
